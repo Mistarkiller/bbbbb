d0af1ac4f505be54824cb0b497007134686724dd
ps21 <mishamorvaniuk@gmail.com>
2024-10-27 11:34:22 +0200

    test

---
+++ main.py
@@ -0,0 +1,8 @@
+class Age:
+    def_init_(self,name,age):
+      self.name = name
+      self.age = age
+
+    def birsday(self)
+      self.age += 1
+      print(f"З днем народження,{self.age}років.")
